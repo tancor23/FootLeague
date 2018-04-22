387697cfea890efc025b01856c36f7208b8ad231
Vydra Sergei <35927520+tancor23@users.noreply.github.com>
2018-04-22 15:21:50 +0300

    Add files via upload
    add web(Validator, Formatter and own Exception)

---
+++ java/by/rebel/footleague/web/util/WebConstantDeclaration.java
@@ -0,0 +1,22 @@
+package by.rebel.footleague.web.util;
+
+public class WebConstantDeclaration {
+	public static final String REQUEST_PARAM_ACTION = "action";
+	public static final String REQUEST_PARAM_CAR_LIST = "carlist";
+
+	public static final String REQUEST_PARAM_USER_ID = "user_id";
+	public static final String REQUEST_PARAM_CAR_ID = "car_id";
+	public static final String REQUEST_MSG_SUCCES = "msg_success";
+
+	public static final String ACTION_NAME_VIEW_CAR_LIST = "view_car_list";
+	public static final String ACTION_NAME_ORDER_CAR = "order_car";
+	public static final String ACTION_NAME_RETURN_CAR = "return_car";
+
+	public static final String PAGE_USER_EDIT = "/WEB-INF/page/user/editcars.jsp";
+	public static final String PAGE_USER_LIST = "/WEB-INF/page/user/main.jsp";
+
+	private WebConstantDeclaration() {
+
+	}
+
+}
