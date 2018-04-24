fee68ff4faf34be228bf66fb7d86d8a5f868dc01
Vydra Sergei <35927520+tancor23@users.noreply.github.com>
2018-04-24 15:25:33 +0300

    Add files via upload
    add all method in MatchDaoImpl.java

--- java/by/rebel/footleague/dao/impl/MatchDaoImpl.java
+++ java/by/rebel/footleague/dao/impl/MatchDaoImpl.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,16 +16,16 @@ import by.rebel.footleague.dao.util.DBConnectionHelper;
 import by.rebel.footleague.domain.Match;
 
 /**
- * @author Vydra_Sergei 
- * Class MatchDaoImpl realizes the methods(create, read, readAll, update, delete).
+ * @author Vydra_Sergei Class MatchDaoImpl realizes the methods(create, read,
+ *         readAll, update, delete).
  */
 public class MatchDaoImpl implements MatchDao {
-	
+
 	private static final Logger logger = LogManager.getLogger();
 	private final String SQL_CREATE_DO_INSERT_INTO_FROM_MATCHES = "INSERT INTO matches (home_team, guest_team) VALUE (?,?);";
-	private final String SQL_READ_DO_SELECT_NEEDS_FROM_MATCHES = "SELECT match_id, name, login, password, email, role FROM matches WHERE match_id=?";
-	private final String SQL_READALL_DO_SELECT_ALL_FROM_MATCHES = "SELECT match_id, name, login, password, email, role FROM matches;";
-	private final String SQL_UPDATE_DO_UPDATE_NEEDS_FROM_MATCHES = "UPDATE matches SET login=?, password=? WHERE match_id=?;";
+	private final String SQL_READ_DO_SELECT_NEEDS_FROM_MATCHES = "SELECT match_id, home_team, guest_team, count_goals_home_team, count_goals_guest_team, points_home_team, points_guest_team FROM matches WHERE match_id=?";
+	private final String SQL_READALL_DO_SELECT_ALL_FROM_MATCHES = "SELECT match_id, home_team, guest_team, count_goals_home_team, count_goals_guest_team, points_home_team, points_guest_team FROM matches;";
+	private final String SQL_UPDATE_DO_UPDATE_NEEDS_FROM_MATCHES = "UPDATE matches SET home_team=?, guest_team=? WHERE match_id=?;";
 	private final String SQL_DELETE_DO_DELETE_ID_FROM_MATCHES = "DELETE FROM matches WHERE match_id=?;";
 
 	@Override
@@ -49,36 +50,61 @@ public class MatchDaoImpl implements MatchDao {
 	@Override
 	public Match read(int matchId) {
 		try (Connection connection = DBConnectionHelper.connect();
-			PreparedStatement ps = connection.prepareStatement(SQL_READ_DO_SELECT_NEEDS_FROM_MATCHES)) {
-		ps.setInt(1, matchId);
-		ResultSet result = ps.executeQuery();
-		if (result.next()) {
-			Match match = new Match(matchId);
-			setMatchFields(match, result);
-			return match;
+				PreparedStatement ps = connection.prepareStatement(SQL_READ_DO_SELECT_NEEDS_FROM_MATCHES)) {
+			ps.setInt(1, matchId);
+			ResultSet result = ps.executeQuery();
+			if (result.next()) {
+				Match match = new Match(matchId);
+				setMatchFields(match, result);
+				return match;
+			}
+		} catch (SQLException e) {
+			logger.error("Error is in MatchDaoImpl.read(); ", e);
 		}
-	} catch (SQLException e) {
-		logger.error("Error is in MatchDaoImpl.read(); ", e);
-	}
-	return null;		
+		return null;
 	}
 
 	@Override
 	public List<Match> readAll() {
+		List<Match> matches = new ArrayList<>();
+		try (Connection connection = DBConnectionHelper.connect(); Statement statement = connection.createStatement()) {
+			ResultSet result = statement.executeQuery(SQL_READALL_DO_SELECT_ALL_FROM_MATCHES);
+			while (result.next()) {
+				Match match = new Match();
+				setMatchFields(match, result);
+				matches.add(match);
+			}
+		} catch (SQLException e) {
+			logger.error("Error is in MatchDaoImpl.readAll(): ", e);
+		}
 		return null;
 	}
 
 	@Override
 	public void update(Match match) {
+		try (Connection connection = DBConnectionHelper.connect();
+				PreparedStatement ps = connection.prepareStatement(SQL_UPDATE_DO_UPDATE_NEEDS_FROM_MATCHES)) {
+			prepareMatchFields(ps, match);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error("Error is in MatchDaoImpl.update(): ", e);
+		}
 	}
 
 	@Override
 	public void delete(int matchId) {
+		try (Connection connection = DBConnectionHelper.connect();
+				PreparedStatement ps = connection.prepareStatement(SQL_DELETE_DO_DELETE_ID_FROM_MATCHES)) {
+			ps.setInt(1, matchId);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error("Error is in MatchDaoImpl.delete(): ", e);
+		}
 	}
 
 	private void setMatchFields(Match match, ResultSet result) throws SQLException {
 		match.setNameTheFirstTeam(result.getString("home_team"));
-		match.setNameTheSecondTeam(result.getString("home_team"));
+		match.setNameTheSecondTeam(result.getString("guest_team"));
 		match.setCountOfGoalsTheFirstTeam(result.getInt("count_goals_home_team"));
 		match.setCountOfGoalsTheSecondTeam(result.getInt("count_goals_guest_team"));
 		match.setPointsHomeTeam(result.getInt("points_home_team"));
@@ -90,5 +116,5 @@ public class MatchDaoImpl implements MatchDao {
 		ps.setString(2, match.getNameTheSecondTeam());
 		ps.setInt(3, match.getId());
 	}
-	
+
 }
