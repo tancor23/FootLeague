db57482c1ac4d2d1c44baf17a7ec903d61c9759a
Vydra Sergei <35927520+tancor23@users.noreply.github.com>
2018-04-28 12:23:53 +0300

    Add files via upload
    add role

--- java/by/rebel/footleague/domain/User.java
+++ java/by/rebel/footleague/domain/User.java
@@ -14,6 +14,7 @@ public class User extends Entity implements Serializable {
 	private String login;
 	private String password;
 	private String email;
+	private Role role;
 
 
 	public User() {
@@ -84,6 +85,8 @@ public class User extends Entity implements Serializable {
 		this.email = email;
 	}
 
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -93,6 +96,7 @@ public class User extends Entity implements Serializable {
 		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
 		return result;
 	}
 
@@ -125,13 +129,18 @@ public class User extends Entity implements Serializable {
 				return false;
 		} else if (!password.equals(other.password))
 			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
 		return "name:" + name + ";  " + "login:" + login + ";  " + "password:" + password + ";  " + "email:" + email
-				+ ";  ";
+				+ ";  " + "role:" + role + ";  ";
 	}
 
 }
